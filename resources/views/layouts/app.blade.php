3dbe3b42a272b4a08e5625b4a7ba5501d7bde897
Bailed <besson.maxime@gmail.com>
2019-03-09 16:30:32 +0100

    ajout google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -94,6 +94,16 @@
             }
         </style>
 
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135897961-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-135897961-1');
+        </script>
+
       </head>
 
       <body>
